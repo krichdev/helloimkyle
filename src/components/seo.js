3d46618b2a66befb4c96510a2b906cd3789541f4
krichdev <kgrichardson2@gmail.com>
2021-03-06 23:20:44 -0800

    no more gif

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import ogImage from "../images/Hello-Im-Kyle.gif";
+import ogImage from "../images/Hello-Im-Kyle.jpg";
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
