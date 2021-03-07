baac2cdd9af7dd745500fb63090f45ff96de40f2
krichdev <kgrichardson2@gmail.com>
2021-03-06 21:16:25 -0800

    postcss config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [require("tailwindcss"), require("autoprefixer")]
-      }
-    },
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
