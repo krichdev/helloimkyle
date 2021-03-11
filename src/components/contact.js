95c9118ed3fdaa3fd2c09be58cbf38660761b91f
krichdev <kgrichardson2@gmail.com>
2021-03-10 17:43:43 -0800

    contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,40 +1,95 @@
-import React from "react"
+import React, { useState } from "react"
 
-const Contact = () => (
-  <section className="bg-indigo-500">
-    <div className="container px-0 py-6 mx-auto sm:px-4 md:w-2/5">
-      <h2 className="text-3xl font-bold text-center text-white">Get In Touch</h2>
-      <div className="mt-8 px-4">
-        <div className="grid grid-cols-1 gap-6">
-          <label className="block">
-            <span className="text-white">Name</span>
-            <input
-              type="text"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="Your Name"
-            />
-          </label>
-          <label className="block">
-            <span className="text-white">Email</span>
-            <input
-              type="email"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="name@example.com"
-            />
-          </label>
-          <label className="block">
-            <span className="text-white">Message</span>
-            <textarea
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              rows="3"
-              placeholder="Write me a message"
-            />
-          </label>
-          <button className="text-white font-semibold px-6 py-3 text-base boorder rounded border-transparent leading-snug bg-indigo-900">Submit</button>
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    submittedMsg: "Thank you for contacting me, I will be in touch soon!",
+  })
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        subject: `Contact Form Submission from Hello I'm Kyle`,
+        ...formData,
+      }),
+    })
+      .then(() =>
+        setFormData({
+          ...formData,
+          submittedMsg:
+            "Thank you for contacting me, I will be in touch soon!",
+        })
+      )
+      .catch(error => alert(error))
+  }
+  return (
+    <section className="bg-indigo-500">
+      <div className="container px-0 py-6 mx-auto sm:px-4 md:w-2/5">
+        <h2 className="text-3xl font-bold text-center text-white">
+          Get In Touch
+        </h2>
+        <div className="mt-8 px-4">
+          <form name="contact" method="POST" data-netlify="true">
+            <div className="grid grid-cols-1 gap-6 h-96">
+              {!formData.submittedMsg ? (
+                <>
+                  <label className="block">
+                    <span className="text-white">Name</span>
+                    <input
+                      type="text"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                      placeholder="Your Name"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-white">Email</span>
+                    <input
+                      type="email"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                      placeholder="name@example.com"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-white">Message</span>
+                    <textarea
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                      rows="3"
+                      placeholder="Write me a message"
+                    />
+                  </label>
+                  <button
+                    type="submit"
+                    className="text-white font-semibold px-6 py-3 text-base boorder rounded border-transparent leading-snug bg-indigo-900"
+                  >
+                    Submit
+                  </button>
+                </>
+              ) : (
+                <p className="text-white text-2xl text-center h-full flex items-center">{formData.submittedMsg}</p>
+              )}
+            </div>
+          </form>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default Contact
