52187e827014b0c3e351059a5acea22bdb9a3183
Kyle Richardson <kylerichardsonnba@gmail.com>
2022-05-26 08:09:49 -0700

    changes

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -20,22 +20,6 @@ const Resume = () => (
       </section>
 
       <div className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
-        {/* <!-- left --> */}
-        <div className="flex flex-row-reverse md:contents">
-          <div className="bg-indigo-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-            <h3 className="font-semibold text-lg mb-1"><a href="https://nba.com" target="_blank">NBA</a></h3>
-            <p className="mb-4 text-xs font-semibold tracking-wider text-white uppercase">March 2022 - Present</p>
-            <p className="leading-tight text-justify">
-              Front end engineer working on custom headless Wordpress features for team site editors, using React and Typescript.
-            </p>
-          </div>
-          <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
-            <div className="h-full w-6 flex items-center justify-center">
-              <div className="h-full w-1 bg-blue-800 pointer-events-none" />
-            </div>
-            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-indigo-500 shadow" />
-          </div>
-        </div>
         {/* <!-- right --> */}
         <div className="flex md:contents">
           <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
